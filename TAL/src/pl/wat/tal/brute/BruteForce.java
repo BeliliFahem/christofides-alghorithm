3cfb68792da4fd8d17be6d1f82abcedd3c59180b
Bartosz Sikorski <k37123@gmail.com>
2013-05-31 17:42:23 +0200

    refs #2 dodanie metody tworzącej pierwszą przykładową drogę, która później będzie permutowana

--- TAL/src/pl/wat/tal/brute/BruteForce.java
+++ TAL/src/pl/wat/tal/brute/BruteForce.java
@@ -1,7 +1,11 @@
 package pl.wat.tal.brute;
 
+import java.util.Iterator;
+import java.util.LinkedList;
+
 import org.jgrapht.WeightedGraph;
 import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 /**
  * 
@@ -16,8 +20,36 @@ public class BruteForce {
 	}
 	
 	public int countRoad(WeightedGraph<String, DefaultEdge> graph, String start, String destination){
+		LinkedList<String> route = createFirstRoute(graph, start, destination);
 		
 		return 0;
+	}
+	
+	/**
+	 * Metoda tworzy pierwsza przykladowa droge
+	 * @author k37
+	 * @param graph graf
+	 * @param start poczatek trasy
+	 * @param destination cel trasy
+	 * @return lista wierzcholkow drogi
+	 */
+	
+	private LinkedList<String> createFirstRoute(WeightedGraph<String, DefaultEdge> graph ,String start, String destination){
+		LinkedList<String> route = new LinkedList<String>();
+		
+		route.add(start);
+		Iterator<String> i = new DepthFirstIterator<String, DefaultEdge>(graph);
+		while(i.hasNext()){
+			String vertex = i.next();
+			if(!vertex.equals(start) && !vertex.equals(destination)){  // musi byc rozny od startu i konca
+				route.add(vertex);  // dodanie kolejnego wierzcholka do drogi
+			}
+		}
+		
+		route.add(destination);
+		route.add(start);
+		
+		return route;
 	}
 
 }
