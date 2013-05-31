79f84897e0bdd2e5b5d4f250ca7f84a2ee0c14c7
Bartosz Sikorski <k37123@gmail.com>
2013-05-31 16:16:03 +0200

    refs #2 Pierwszy wstępny szkielet klasy BruteForce

---
+++ TAL/src/pl/wat/tal/brute/BruteForce.java
@@ -0,0 +1,23 @@
+package pl.wat.tal.brute;
+
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.graph.DefaultEdge;
+
+/**
+ * 
+ * @author k37
+ *
+ */
+
+public class BruteForce {
+	
+	public BruteForce(){
+		
+	}
+	
+	public int countRoad(WeightedGraph<String, DefaultEdge> graph, String start, String destination){
+		
+		return 0;
+	}
+
+}
