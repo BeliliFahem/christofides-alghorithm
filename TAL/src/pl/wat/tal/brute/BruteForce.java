f9bc58298edf6bee4e1f9e5095b1f0884d1712ae
Ignacy Sawicki <igesef@gmail.com>
2013-05-31 21:50:09 +0200

    #5 Test Dla BruteForce

--- TAL/src/pl/wat/tal/brute/BruteForce.java
+++ TAL/src/pl/wat/tal/brute/BruteForce.java
@@ -1,128 +1,126 @@
 package pl.wat.tal.brute;
 
+import org.jgrapht.WeightedGraph;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.traverse.DepthFirstIterator;
+import pl.wat.tal.misc.TSPResult;
+
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
 
-import org.jgrapht.WeightedGraph;
-import org.jgrapht.graph.DefaultEdge;
-import org.jgrapht.traverse.DepthFirstIterator;
-
-import pl.wat.tal.misc.TSPResult;
-
 /**
- * 
  * @author k37
- *
  */
 
 public class BruteForce {
-	private WeightedGraph<String, DefaultEdge> graph;
-	private long bestDistance;
-	
-	public BruteForce(WeightedGraph<String, DefaultEdge> graph){
-		this.graph = graph;
-		bestDistance = 0;
-	}
-	
-	/**
-	 * 
-	 * @param start poczatek trasy
-	 * @param destination cel trasy
-	 * @return droga + obliczona odleglosc
-	 */
-	
-	public TSPResult countRoad(String start, String destination){
-		TSPResult result = new TSPResult();
-		List<String> route = createFirstRoute(start, destination);
-		bestDistance = countDistance(route);  // pierwsza odleglosc
-		
-		List<LinkedList<String>> permutations;  // TODO znalezienie permutacji
-		
-		return result;
-	}
-	
-	/**
-	 * Metoda tworzy pierwsza przykladowa droge
-	 * @author k37
-	 * @param start poczatek trasy
-	 * @param destination cel trasy
-	 * @return lista wierzcholkow drogi
-	 */
-	
-	private List<String> createFirstRoute(String start, String destination){
-		List<String> route = new LinkedList<String>();
-		
-		route.add(start);
-		Iterator<String> i = new DepthFirstIterator<String, DefaultEdge>(graph);
-		while(i.hasNext()){
-			String vertex = i.next();
-			if(!vertex.equals(start) && !vertex.equals(destination)){  // musi byc rozny od startu i konca
-				route.add(vertex);  // dodanie kolejnego wierzcholka do drogi
-			}
-		}
-		
-		route.add(destination);
-		route.add(start);
-		
-		return route;
-	}
-	
-	/**
-	 * Metoda zwraca wszystkie permutacje zbioru. Zachowuje kolejnosc pierwszego i dwoch ostatnich elementow.
-	 * @author k37
-	 * @param route lista z przykladowa droga
-	 * @param index aktualny element permutacji
-	 * @param permutations lista zwrotna
-	 */
-	
-	private void permute(List<String> route, int index, List<LinkedList<String>> permutations){
-		//List<LinkedList<String>> permutations = new LinkedList<LinkedList<String>>();
-		
-		if(index >= route.size() - 2){  // koniec permutacji
-			permutations.add((LinkedList<String>) route);
-		}
-		else{
-			String element = route.get(index);
-			for(int i = index + 1; i < route.size() - 2; i++){
-				// zamiana elemntow na pozycjach index oraz i
-				route.set(index, route.get(i));
-				route.set(i, element);
-				
-				// rekurencja
-				permute(route, index+1, permutations);
-				
-				// powrot do poprzedniej postaci
-				route.set(i, route.get(index));
-				route.set(index, element);
-			}
-		}
-		
-	}
-	
-	/**
-	 * Metoda obliczajaca dlugosc drogi
-	 * @author k37
-	 * @param route lista zawierajaca droge
-	 * @return dlugosc drogi
-	 */
-	
-	private long countDistance(List<String> route){
-		long result = 0;
-		String from = new String();
-		String to = new String();
-		
-		Iterator<String> i = route.iterator();
-		from = i.next();  // pierwszy wierzcholek
-		to = i.next();  // pierwsze odwiedzane miasto
-		
-		while(i.hasNext()){
-			result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // dodanie wagi obecnej krawedzi
-			from = to;  // zamiana koncowego na poczatkowy
-			to = i.next();  // pobranie kolejnego wierzcholka
-		}
-		
-		result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // ostatni wierzcholek poza petla
-		return result;
-	}
+    private WeightedGraph<String, DefaultEdge> graph;
+    private long bestDistance;
+
+    public BruteForce(WeightedGraph<String, DefaultEdge> graph) {
+        this.graph = graph;
+        bestDistance = 0;
+    }
+
+    /**
+     * @param start       poczatek trasy
+     * @param destination cel trasy
+     * @return droga + obliczona odleglosc
+     */
+
+    public TSPResult countRoad(String start, String destination) {
+        TSPResult result = new TSPResult();
+        List<String> route = createFirstRoute(start, destination);
+        bestDistance = countDistance(route);  // pierwsza odleglosc
+
+        List<LinkedList<String>> permutations;  // TODO znalezienie permutacji
+
+        return result;
+    }
+
+    /**
+     * Metoda tworzy pierwsza przykladowa droge
+     *
+     * @param start       poczatek trasy
+     * @param destination cel trasy
+     * @return lista wierzcholkow drogi
+     * @author k37
+     */
+
+    private List<String> createFirstRoute(String start, String destination) {
+        List<String> route = new LinkedList<String>();
+
+        route.add(start);
+        Iterator<String> i = new DepthFirstIterator<String, DefaultEdge>(graph);
+        while (i.hasNext()) {
+            String vertex = i.next();
+            if (!vertex.equals(start) && !vertex.equals(destination)) {  // musi byc rozny od startu i konca
+                route.add(vertex);  // dodanie kolejnego wierzcholka do drogi
+            }
+        }
+
+        route.add(destination);
+        route.add(start);
+
+        return route;
+    }
+
+    /**
+     * Metoda zwraca wszystkie permutacje zbioru. Zachowuje kolejnosc pierwszego i dwoch ostatnich elementow.
+     *
+     * @param route        lista z przykladowa droga
+     * @param index        aktualny element permutacji
+     * @param permutations lista zwrotna
+     * @author k37
+     */
+
+    public void permute(List<String> route, int index, List<LinkedList<String>> permutations) {
+        //List<LinkedList<String>> permutations = new LinkedList<LinkedList<String>>();
+
+        if (index >= route.size() - 2) {  // koniec permutacji
+            permutations.add((LinkedList<String>) route);
+        } else {
+            String element = route.get(index);
+            for (int i = index + 1; i < route.size() - 2; i++) {
+                // zamiana elemntow na pozycjach index oraz i
+                route.set(index, route.get(i));
+                route.set(i, element);
+
+                // rekurencja
+                permute(route, index + 1, permutations);
+
+                // powrot do poprzedniej postaci
+                route.set(i, route.get(index));
+                route.set(index, element);
+            }
+        }
+
+    }
+
+    /**
+     * Metoda obliczajaca dlugosc drogi
+     *
+     * @param route lista zawierajaca droge
+     * @return dlugosc drogi
+     * @author k37
+     */
+
+    private long countDistance(List<String> route) {
+        long result = 0;
+        String from = new String();
+        String to = new String();
+
+        Iterator<String> i = route.iterator();
+        from = i.next();  // pierwszy wierzcholek
+        to = i.next();  // pierwsze odwiedzane miasto
+
+        while (i.hasNext()) {
+            result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // dodanie wagi obecnej krawedzi
+            from = to;  // zamiana koncowego na poczatkowy
+            to = i.next();  // pobranie kolejnego wierzcholka
+        }
+
+        result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // ostatni wierzcholek poza petla
+        return result;
+    }
 }
---
+++ TAL/src/pl/wat/tal/tests/BruteForceTest.java
@@ -0,0 +1,96 @@
+package pl.wat.tal.tests;
+
+import junit.framework.Assert;
+import org.jgrapht.graph.DefaultEdge;
+import org.jgrapht.graph.DefaultWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import org.junit.Test;
+import pl.wat.tal.brute.BruteForce;
+
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Ignacy Sawicki
+ */
+public class BruteForceTest {
+    @Test
+    public void testPermute() throws Exception {
+        BruteForce bruteForce = new BruteForce(new SimpleWeightedGraph<String, DefaultEdge>(DefaultWeightedEdge.class));
+//        Method method = BruteForce.class.getDeclaredMethod("permute", String.class);
+//        method.setAccessible(true);
+        List<String> list = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Warszawa");
+        list.add("Katowice");
+        list.add("Berlin");
+        list.add("Lodz");
+
+        List result = new LinkedList<LinkedList<String>>();
+        bruteForce.permute(list, 1, result);
+//        method.invoke(bruteForce, list);
+
+        List<String> list1 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Katowice");
+        list.add("Warszawa");
+        list.add("Berlin");
+        list.add("Lodz");
+
+        List<String> list2 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Katowice");
+        list.add("Berlin");
+        list.add("Warszawa");
+        list.add("Lodz");
+
+        List<String> list3 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Warszawa");
+        list.add("Katowice");
+        list.add("Berlin");
+        list.add("Lodz");
+
+        List<String> list4 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Warszawa");
+        list.add("Berlin");
+        list.add("Katowice");
+        list.add("Lodz");
+
+        List<String> list5 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Berlin");
+        list.add("Katowice");
+        list.add("Warszawa");
+        list.add("Lodz");
+
+        List<String> list6 = new LinkedList<String>();
+        list.add("Lodz");
+        list.add("Berlin");
+        list.add("Warszawa");
+        list.add("Katowice");
+        list.add("Lodz");
+
+        Assert.assertTrue(containsList(result, list1));
+        Assert.assertTrue(containsList(result, list2));
+        Assert.assertTrue(containsList(result, list3));
+        Assert.assertTrue(containsList(result, list4));
+        Assert.assertTrue(containsList(result, list5));
+        Assert.assertTrue(containsList(result, list6));
+    }
+
+    /**
+     * @param haystack
+     * @param needle
+     * @return
+     */
+    private boolean containsList(List<List> haystack, List<String> needle) {
+        for (List list : haystack) {
+            if (list.equals(needle)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
