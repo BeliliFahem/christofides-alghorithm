2a9be9d39e8030356b8b2a1225f3c67249fc6ae9
Bartosz Sikorski <k37123@gmail.com>
2013-05-31 19:47:27 +0200

    refs #2 dodanie metody liczacej odleglosc + drobne poprawki

--- TAL/src/pl/wat/tal/brute/BruteForce.java
+++ TAL/src/pl/wat/tal/brute/BruteForce.java
@@ -2,6 +2,7 @@ package pl.wat.tal.brute;
 
 import java.util.Iterator;
 import java.util.LinkedList;
+import java.util.List;
 
 import org.jgrapht.WeightedGraph;
 import org.jgrapht.graph.DefaultEdge;
@@ -16,22 +17,22 @@ import pl.wat.tal.misc.TSPResult;
  */
 
 public class BruteForce {
+	private WeightedGraph<String, DefaultEdge> graph;
 	
-	public BruteForce(){
-		
+	public BruteForce(WeightedGraph<String, DefaultEdge> graph){
+		this.graph = graph;
 	}
 	
 	/**
 	 * 
-	 * @param graph graf
 	 * @param start poczatek trasy
 	 * @param destination cel trasy
 	 * @return droga + obliczona odleglosc
 	 */
 	
-	public TSPResult countRoad(WeightedGraph<String, DefaultEdge> graph, String start, String destination){
+	public TSPResult countRoad(String start, String destination){
 		TSPResult result = new TSPResult();
-		LinkedList<String> route = createFirstRoute(graph, start, destination);
+		List<String> route = createFirstRoute(start, destination);
 		
 		return result;
 	}
@@ -39,14 +40,13 @@ public class BruteForce {
 	/**
 	 * Metoda tworzy pierwsza przykladowa droge
 	 * @author k37
-	 * @param graph graf
 	 * @param start poczatek trasy
 	 * @param destination cel trasy
 	 * @return lista wierzcholkow drogi
 	 */
 	
-	private LinkedList<String> createFirstRoute(WeightedGraph<String, DefaultEdge> graph, String start, String destination){
-		LinkedList<String> route = new LinkedList<String>();
+	private List<String> createFirstRoute(String start, String destination){
+		List<String> route = new LinkedList<String>();
 		
 		route.add(start);
 		Iterator<String> i = new DepthFirstIterator<String, DefaultEdge>(graph);
@@ -70,9 +70,35 @@ public class BruteForce {
 	 * @return lista wszystkich mozliwych drog
 	 */
 	
-	private LinkedList<LinkedList<String>> permute(LinkedList<String> route){
-		LinkedList<LinkedList<String>> permutations = new LinkedList<LinkedList<String>>();
+	private List<LinkedList<String>> permute(List<String> route){
+		List<LinkedList<String>> permutations = new LinkedList<LinkedList<String>>();
 		
 		return permutations;
+	}
+	
+	/**
+	 * Metoda obliczajaca dlugosc drogi
+	 * @author k37
+	 * @param route lista zawierajaca droge
+	 * @return dlugosc drogi
+	 */
+	
+	private long countDistance(List<String> route){
+		long result = 0;
+		String from = new String();
+		String to = new String();
+		
+		Iterator<String> i = route.iterator();
+		from = i.next();  // pierwszy wierzcholek
+		to = i.next();  // pierwsze odwiedzane miasto
+		
+		while(i.hasNext()){
+			result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // dodanie wagi obecnej krawedzi
+			from = to;  // zamiana koncowego na poczatkowy
+			to = i.next();  // pobranie kolejnego wierzcholka
+		}
+		
+		result = (long) (result + graph.getEdgeWeight(graph.getEdge(from, to)));  // ostatni wierzcholek poza petla
+		return result;
 	}
 }
