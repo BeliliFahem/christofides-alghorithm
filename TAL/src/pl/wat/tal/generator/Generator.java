4cae0b8118b0e0535995e0f839b9ba07d4a32474
Bartosz Sikorski <k37123@gmail.com>
2013-06-01 15:27:38 +0200

    refs #1 generowanie wierzcholkow

--- TAL/src/pl/wat/tal/generator/Generator.java
+++ TAL/src/pl/wat/tal/generator/Generator.java
@@ -11,6 +11,11 @@ import org.jgrapht.graph.SimpleWeightedGraph;
  */
 
 public class Generator {
+	public final static int GAUSSIAN = 0;
+	public final static int POISSON = 1;
+	public final static int BINOMIAL = 2;
+	public final static int UNIFORM = 3;
+	public final static int EXPOTENTIAL = 4;
 	
 	public Generator(){
 		
@@ -19,14 +24,40 @@ public class Generator {
 	/**
 	 * Metoda generujaca pelny graf wazony
 	 * @param vertices liczba wierzcholkow
+	 * @param trueNames nazwy wierzcholkow pobierane z listy
 	 * @param minWeight minimalna odleglosc miedzy wierzcholkami
 	 * @param maxWeight maksymalna odleglosc miedzy wierzcholkami
+	 * @param seed
+	 * @param distribution rozklad generowanych odleglosci
 	 * @return graf
 	 */
 	
-	public WeightedGraph<String, DefaultWeightedEdge> generate(int vertices, int minWeight, int maxWeight){
-		SimpleWeightedGraph<String, DefaultWeightedEdge> graph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+	public WeightedGraph<String, DefaultWeightedEdge> generate(int vertices, boolean trueNames, int minWeight, int maxWeight, long seed, int distribution){
+		WeightedGraph<String, DefaultWeightedEdge> graph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
 		
+		graph = generateVerices(vertices, trueNames);
+		
+		return graph;
+	}
+	
+	/**
+	 * Metoda pomocnicza generujaca wierzcholki w grafie
+	 * @param number liczba wierzcholkow
+	 * @param trueNames czy nazwy wierzcholkow z listy
+	 * @return graf z wierzcholkami
+	 */
+	
+	private WeightedGraph<String, DefaultWeightedEdge> generateVerices(int number, boolean trueNames){
+		WeightedGraph<String, DefaultWeightedEdge> graph = new SimpleWeightedGraph<String, DefaultWeightedEdge>(DefaultWeightedEdge.class);
+		
+		if(trueNames){
+			// TODO
+		}
+		else{
+			for(int i=0; i<number; i++){
+				graph.addVertex("v" + i + 1);
+			}
+		}
 		return graph;
 	}
 
