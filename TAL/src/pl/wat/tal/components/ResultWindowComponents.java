8247fb4e03d204bc6c5e4ad109d7c558edfedc3f
Ignacy Sawicki <igesef@gmail.com>
2013-06-22 15:25:25 +0200

    refs #10 Poprawienie importów

--- TAL/src/pl/wat/tal/components/ResultWindowComponents.java
+++ TAL/src/pl/wat/tal/components/ResultWindowComponents.java
@@ -1,103 +1,92 @@
 package pl.wat.tal.components;
 
-import java.awt.BorderLayout;
-import java.awt.GridLayout;
+import org.jgrapht.graph.AdvancedWeightedEdge;
+import org.jgrapht.graph.SimpleWeightedGraph;
+import pl.wat.tal.brute.BruteForce;
+import pl.wat.tal.chris.ChristofidesAlgorithm;
+import pl.wat.tal.common.Algorithm;
+import pl.wat.tal.misc.TSPResult;
+
+import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Calendar;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-
-import org.jgrapht.graph.SimpleWeightedGraph;
-
-import pl.wat.tal.brute.BruteForce;
-import pl.wat.tal.chris.ChristofidesAlgorithm;
-import pl.wat.tal.common.AdvancedWeightedEdge;
-import pl.wat.tal.common.Algorithm;
-import pl.wat.tal.misc.TSPResult;
-
 public class ResultWindowComponents implements ActionListener {
-	private JFrame operatingWindow;
-	private JTextArea results;
-	private JScrollPane resultsScroller;
-	private JButton ok;
-	private JButton save;
-	private JPanel centralPane;
-	private JPanel bottomPane;
-	private BorderLayout centralLayout;
-	private GridLayout bottomLayout;
-	private Algorithm algorithm;
-	
-	public ResultWindowComponents(JFrame operatingWindow){
-		this.operatingWindow = operatingWindow;
-		initCentralPanel();
-		initBottomPanel();
-	}
-	
-	protected void initCentralPanel(){
-		results = new JTextArea(200, 100);
-		resultsScroller = new JScrollPane(results);
-	}
-	
-	protected void initBottomPanel(){
-		bottomPane = new JPanel();
-		bottomLayout = new GridLayout(2, 1, 20, 20);
-		
-		save = new JButton("Zapisz do pliku");
-		ok = new JButton("OK");
-		
-		save.addActionListener(this);
-		ok.addActionListener(this);
-		
-		bottomPane.setLayout(bottomLayout);
-		bottomPane.add(save);
-		bottomPane.add(ok);
-	}
-	
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		JButton source = (JButton) e.getSource();
-		if(source == save){
-			// TODO
-		}
-		else
-			if(source == ok){
-				operatingWindow.setVisible(false);  // nie zamykamy okna chowamy je tylko
-			}
-		
-	}
-	
-	public void countRoad(int algorithm, String startVertex, SimpleWeightedGraph<String, AdvancedWeightedEdge> graph){
-		results.setText(results.getText() + "\n\n" + "====================" + "\n");
-		results.setText(results.getText() + "Data: " + Calendar.DAY_OF_MONTH + "." + Calendar.MONTH + 1 + "." + Calendar.YEAR + "  " + Calendar.HOUR_OF_DAY + ":" + Calendar.MINUTE + "\n");
-		
-		if(algorithm == 0){
-			results.setText(results.getText() + "U¿yto algorytmu brute force \n");
-			this.algorithm = new BruteForce();
-		}
-		else
-			if(algorithm == 1){
-				results.setText(results.getText() + "U¿yto algorytmu Christofidesa \n");
-				this.algorithm = new ChristofidesAlgorithm();
-			}
-		
-		TSPResult result = this.algorithm.findSolution(startVertex, graph);
-		results.setText(results.getText() + "Wybrany wierzcho³ek pocz¹tkowy: " + startVertex + "\n");
-		results.setText(results.getText() + "Wyznaczona droga: " + result.getRoute().toString() + "\n");
-		results.setText(results.getText() + "Obliczona d³ugoœæ: " + result.getDistance() + "\n");
-		results.setText(results.getText() + "====================" + "\n");
-	}
+    private JFrame operatingWindow;
+    private JTextArea results;
+    private JScrollPane resultsScroller;
+    private JButton ok;
+    private JButton save;
+    private JPanel centralPane;
+    private JPanel bottomPane;
+    private BorderLayout centralLayout;
+    private GridLayout bottomLayout;
+    private Algorithm algorithm;
 
-	public JScrollPane getResultsScroller() {
-		return resultsScroller;
-	}
+    public ResultWindowComponents(JFrame operatingWindow) {
+        this.operatingWindow = operatingWindow;
+        initCentralPanel();
+        initBottomPanel();
+    }
 
-	public JPanel getBottomPane() {
-		return bottomPane;
-	}
+    protected void initCentralPanel() {
+        results = new JTextArea(200, 100);
+        resultsScroller = new JScrollPane(results);
+    }
+
+    protected void initBottomPanel() {
+        bottomPane = new JPanel();
+        bottomLayout = new GridLayout(2, 1, 20, 20);
+
+        save = new JButton("Zapisz do pliku");
+        ok = new JButton("OK");
+
+        save.addActionListener(this);
+        ok.addActionListener(this);
+
+        bottomPane.setLayout(bottomLayout);
+        bottomPane.add(save);
+        bottomPane.add(ok);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        JButton source = (JButton) e.getSource();
+        if (source == save) {
+            // TODO
+        } else if (source == ok) {
+            operatingWindow.setVisible(false);  // nie zamykamy okna chowamy je tylko
+        }
+
+    }
+
+    public void countRoad(int algorithm, String startVertex, SimpleWeightedGraph<String, AdvancedWeightedEdge> graph) {
+        results.setText(results.getText() + "\n\n" + "====================" + "\n");
+        results.setText(results.getText() + "Data: " + Calendar.DAY_OF_MONTH + "." + Calendar.MONTH + 1 + "." + Calendar.YEAR + "  " + Calendar.HOUR_OF_DAY + ":" + Calendar.MINUTE + "\n");
+
+        if (algorithm == 0) {
+            results.setText(results.getText() + "Uï¿½yto algorytmu brute force \n");
+            this.algorithm = new BruteForce();
+        } else if (algorithm == 1) {
+            results.setText(results.getText() + "Uï¿½yto algorytmu Christofidesa \n");
+            this.algorithm = new ChristofidesAlgorithm();
+        }
+
+        TSPResult result = this.algorithm.findSolution(startVertex, graph);
+        results.setText(results.getText() + "Wybrany wierzchoï¿½ek poczï¿½tkowy: " + startVertex + "\n");
+        results.setText(results.getText() + "Wyznaczona droga: " + result.getRoute().toString() + "\n");
+        results.setText(results.getText() + "Obliczona dï¿½ugoï¿½ï¿½: " + result.getDistance() + "\n");
+        results.setText(results.getText() + "====================" + "\n");
+    }
+
+    public JScrollPane getResultsScroller() {
+        return resultsScroller;
+    }
+
+    public JPanel getBottomPane() {
+        return bottomPane;
+    }
 
 }
--- TAL/src/pl/wat/tal/view/ResultWindow.java
+++ TAL/src/pl/wat/tal/view/ResultWindow.java
@@ -1,45 +1,42 @@
 package pl.wat.tal.view;
 
-import java.awt.BorderLayout;
-import java.awt.Container;
-
-import javax.swing.JFrame;
-
+import org.jgrapht.graph.AdvancedWeightedEdge;
 import org.jgrapht.graph.SimpleWeightedGraph;
-
-import pl.wat.tal.common.AdvancedWeightedEdge;
 import pl.wat.tal.components.ResultWindowComponents;
+
+import javax.swing.*;
+import java.awt.*;
 
 public class ResultWindow extends JFrame {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 5694842464899873963L;
-	private BorderLayout layout;
-	private ResultWindowComponents rwc;
-	public final static int BRUTE = 0;
-	public final static int CHRISTOFIDES = 1;
-	
-	public ResultWindow(){
-		rwc = new ResultWindowComponents(this);
-		layout = new BorderLayout();
-		Container c = this.getContentPane();
-		c.setLayout(layout);
-		
-		c.add(rwc.getResultsScroller(), BorderLayout.CENTER);
-		c.add(rwc.getBottomPane(), BorderLayout.PAGE_END);
-		
-		setTitle("Projekt TAL");
-		setVisible(true);
-	    setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
-	    pack();
-	    setLocationRelativeTo(null);
-	}
-	
-	public void count(int algorithm, String startVertex, SimpleWeightedGraph<String, AdvancedWeightedEdge> graph){
-		this.setVisible(true);
-		rwc.countRoad(algorithm, startVertex, graph);
-	}
+    /**
+     *
+     */
+    private static final long serialVersionUID = 5694842464899873963L;
+    private BorderLayout layout;
+    private ResultWindowComponents rwc;
+    public final static int BRUTE = 0;
+    public final static int CHRISTOFIDES = 1;
+
+    public ResultWindow() {
+        rwc = new ResultWindowComponents(this);
+        layout = new BorderLayout();
+        Container c = this.getContentPane();
+        c.setLayout(layout);
+
+        c.add(rwc.getResultsScroller(), BorderLayout.CENTER);
+        c.add(rwc.getBottomPane(), BorderLayout.PAGE_END);
+
+        setTitle("Projekt TAL");
+        setVisible(true);
+        setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+    }
+
+    public void count(int algorithm, String startVertex, SimpleWeightedGraph<String, AdvancedWeightedEdge> graph) {
+        this.setVisible(true);
+        rwc.countRoad(algorithm, startVertex, graph);
+    }
 
 }
